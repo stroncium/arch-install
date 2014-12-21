a2f2a9afa2b602366d2c588b67d0da194b5289a6
Yanis Benson <yanis.benson@gmail.com>
2014-12-21 04:09:41 +0300

    Update vb-my.sh

--- vb-my.sh
+++ vb-my.sh
@@ -32,6 +32,7 @@ mount /dev/sda1 $ARCH_ROOT/boot
 
 STEP PACSTRAP
 #pacstrap $ARCH_ROOT base base-devel grub sudo wget postfix net-tools
+pacstrap $ARCH_ROOT base grub wget net-tools
  
 STEP MAKING FSTAB
 genfstab -p $ARCH_ROOT >> $ARCH_ROOT/etc/fstab
