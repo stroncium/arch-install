9636398ce342215144d24a6db619e25054077b14
Yanis Benson <yanis.benson@gmail.com>
2014-12-21 06:44:35 +0300

    Update my-vb-post.sh

--- my-vb-post.sh
+++ my-vb-post.sh
@@ -6,4 +6,37 @@ amixer -s<<EOF
   sset PCM 100%
 EOF
 alsactl store
-speaker-test -c2
+#speaker-test -c2
+
+cp /usr/lib/systemd/system/getty@.service /etc/systemd/system/autologin@.service
+sed -i -e "/ExecStart/s/%I/-a $ARCH_USER %I/" /etc/systemd/system/autologin@.service
+systemctl daemon-reload
+systemctl disable getty@tty1
+systemctl enable autologin@tty1
+
+cp /etc/skel/.bash_profile /home/$ARCH_USER/.bash_profile
+echo '[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && exec startx' >> /home/$ARCH_USER/.bash_profile
+
+cat > /home/$ARCH_USER/.xinitrc <<EOF
+/usr/bin/VBoxClient-all
+exec startlxde
+EOF
+mkdir builds
+cd builds
+
+wget https://aur.archlinux.org/packages/pa/package-query/package-query.tar.gz
+tar -xf package-query.tar.gz
+cd package-query
+makepkg --asroot
+yes | pacman -U package-query-*.pkg.tar.*
+cd ..
+
+wget https://aur.archlinux.org/packages/ya/yaourt/yaourt.tar.gz
+tar -xf yaourt.tar.gz
+cd yaourt
+makepkg --asroot
+yes | pacman -U yaourt-*.pkg.tar.*
+cd ..
+
+cd ..
+rm -rf builds
